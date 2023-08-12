5d80155094415caedc180da3e739bcc4e3ec1896
jirkakrepl <jiri.krepl@gmail.com>
2023-08-12 20:28:48 +0200

    feat: clear uart buffer, so only 32 bytes are received

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ uart = UART(1, 9600)
 def read_sensor_data():
     data = bytearray()
     while True:
-        if uart.any():
+        uart_bytes_length = uart.any()
+        # print('uart_bytes_lenth:', uart_bytes)
+        if uart_bytes_length:
             first_byte = uart.read(1)
+            # print('first_byte:', first_byte)
             if first_byte == START_CHARACTER_1:
                 data += first_byte
                 second_byte = uart.read(1)
+                # print('second_byte:', second_byte)
                 if second_byte == START_CHARACTER_2:
                     data += second_byte
                     data += uart.read(30)
+                    # Clear any remaining bytes in the UART buffer
+                    while uart.any():
+                        uart.read(1)
                     return data
 
 def extract_pm_concentration(data, low_byte_index, high_byte_index):
@@ -29,7 +36,7 @@ while True:
     try:
         sensor_data = read_sensor_data()
         # Process sensor_data here
-        print("Sensor data:", sensor_data)
+        # print("Sensor data:", sensor_data)
         pm1_concentration = extract_pm_concentration(sensor_data, 4, 5)
         pm25_concentration = extract_pm_concentration(sensor_data, 6, 7)
         pm10_concentration = extract_pm_concentration(sensor_data, 8, 9)
@@ -39,4 +46,4 @@ while True:
         print()  # Print a new line
     except Exception as e:
         print("Error:", e)
-    time.sleep(5)
+    time.sleep(1)
