5a6b1a0b21f9e938a252653a555e084cd0a48e35
jirkakrepl <jiri.krepl@gmail.com>
2023-08-13 09:55:30 +0200

    feat: add code to initiate downloaded pms7003 class

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from Pms7003 import Pms7003 as Pms7003Class
+
+pms_instance = Pms7003Class(1)
+
+while True:
+    try:
+        sensor_data = pms_instance.read()
+        PM1_0 = sensor_data['PM1_0']
+        PM2_5 = sensor_data['PM2_5']
+        PM10_0 = sensor_data['PM10_0']
+        print('PM1.0:', PM1_0, 'PM2.5:', PM2_5, 'PM10:', PM10_0)
+    except Exception as e:
+        print("Error:", e)
+    time.sleep(1)
