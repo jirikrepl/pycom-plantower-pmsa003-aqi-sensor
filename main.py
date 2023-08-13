ab1dbf296aa12cec13136189daa4b9e5b01aade1
jirkakrepl <jiri.krepl@gmail.com>
2023-08-13 11:33:09 +0200

    refactor: better print message for PM values

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
         PM1_0 = sensor_data['PM1_0']
         PM2_5 = sensor_data['PM2_5']
         PM10_0 = sensor_data['PM10_0']
-        print('PM1.0:', PM1_0, 'PM2.5:', PM2_5, 'PM10:', PM10_0)
+        print('PM1.0: {} μg/m³\t PM2.5: {} μg/m³\t PM10: {} μg/m³'.format(PM1_0, PM2_5, PM10_0))
     except Exception as e:
         print("Error:", e)
     time.sleep(1)
