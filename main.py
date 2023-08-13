54cdb0735759559c86ef2ef7c0e68adefec2b40b
jirkakrepl <jiri.krepl@gmail.com>
2023-08-13 11:37:44 +0200

    refactor: use atmospheric PM values
    - this should be used in real world measurements

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ pms_instance = Pms7003Class(1)
 while True:
     try:
         sensor_data = pms_instance.read()
-        PM1_0 = sensor_data['PM1_0']
-        PM2_5 = sensor_data['PM2_5']
-        PM10_0 = sensor_data['PM10_0']
+        PM1_0 = sensor_data['PM1_0_ATM']
+        PM2_5 = sensor_data['PM2_5_ATM']
+        PM10_0 = sensor_data['PM10_0_ATM']
         print('PM1.0: {} μg/m³\t PM2.5: {} μg/m³\t PM10: {} μg/m³'.format(PM1_0, PM2_5, PM10_0))
     except Exception as e:
         print("Error:", e)
