d78c15175ff187bb5bd4453e7ad342cd60de0d37
jirkakrepl <jiri.krepl@gmail.com>
2023-12-22 13:40:48 +0100

    refactor: print average co2 values only after defined time interval

--- co2/HC8/HC8SensorActiveOutputGpt.ino
+++ co2/HC8/HC8SensorActiveOutputGpt.ino
@@ -18,12 +18,39 @@ SoftwareSerial sensorSerial(2, 3);  // RX, TX
 // Define the number of bytes in a data packet
 #define DATA_PACKET_SIZE 16
 
+// Define the size of the CO2 buffer in "element-seconds"
+// The CO2 sensor sends data every second,
+// and the CO2_BUFFER_SIZE determines the number of measurements stored.
+// It is also used as the interval (in seconds) for printing the average value.
+#define CO2_BUFFER_SIZE 60
+unsigned long intervalPrintTime = CO2_BUFFER_SIZE * 1000UL;
+
+// Initialize an array to store CO2 values
+uint16_t co2Values[CO2_BUFFER_SIZE];
+// Variable to keep track of the current index in the buffer
+int co2Index = 0;
+// Variable to keep track of the number of valid measurements in the buffer
+int validMeasurements = 0;
+// Variable to keep track of the last print time
+unsigned long lastPrintTime = 0;
+
 void setup() {
   // Start the serial communication with the CO2 sensor
   sensorSerial.begin(9600);
   Serial.begin(9600);  // You can change this baud rate based on your needs
+}
 
-  Serial.println("CO2 Sensor Communication Debug");
+void printAverage() {
+  // Calculate the average of the valid CO2 values in the buffer
+  uint32_t sum = 0;
+  for (int i = 0; i < validMeasurements; i++) {
+    sum += co2Values[i];
+  }
+  uint16_t average = static_cast<uint16_t>(sum / validMeasurements);
+
+  // Print the average CO2 concentration
+  Serial.print("Average CO2 Concentration: ");
+  Serial.println(average);
 }
 
 void loop() {
@@ -31,13 +58,6 @@ void loop() {
     // Read the data packet from the CO2 sensor
     uint8_t dataPacket[DATA_PACKET_SIZE];
     sensorSerial.readBytes(dataPacket, DATA_PACKET_SIZE);
-
-    Serial.print("Received Data Packet: ");
-    for (int i = 0; i < DATA_PACKET_SIZE; i++) {
-      Serial.print(dataPacket[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.println();
 
     // Check if the data packet has a valid header
     if (dataPacket[0] == 0x42 && dataPacket[1] == 0x4D) {
@@ -50,15 +70,23 @@ void loop() {
         checksum += dataPacket[i];
       }
 
-      Serial.print("Checksum Calculated: ");
-      Serial.println(checksum, HEX);
-      Serial.print("Checksum Received: ");
-      Serial.println(dataPacket[DATA_PACKET_SIZE - 1], HEX);
-
       if (checksum == dataPacket[DATA_PACKET_SIZE - 1]) {
-        // Data is valid, print CO2 concentration
-        Serial.print("CO2 Concentration: ");
-        Serial.println(co2Concentration);
+        // Data is valid, store CO2 concentration in the buffer
+        co2Values[co2Index] = co2Concentration;
+        co2Index = (co2Index + 1) % CO2_BUFFER_SIZE;
+
+        // Update the number of valid measurements in the buffer
+        validMeasurements = min(validMeasurements + 1, CO2_BUFFER_SIZE);
+
+        // Print CO2 concentration
+        // Serial.print("CO2 Concentration: ");
+        // Serial.println(co2Concentration);
+
+        // Check if it's time to print the average
+        if ((millis() - lastPrintTime >= intervalPrintTime) || (millis() < lastPrintTime)) {
+          printAverage();
+          lastPrintTime = millis();
+        }
       } else {
         // Checksum error
         Serial.println("Checksum error!");
@@ -67,7 +95,5 @@ void loop() {
       // Invalid header
       Serial.println("Invalid data packet header!");
     }
-
-    Serial.println();
   }
 }
